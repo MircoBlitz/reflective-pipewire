b12b7ef63d40a871b2bfdf59e4a22c5bb5baee0e
Mirco Blitz <mirco@edelhack.de>
2026-03-18 20:04:10 +0100

    Move both lines higher for full visibility

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -64,8 +64,8 @@ fn title_svg(opts: &TitleOpts) -> String {
         _ => 4 + opts.size as i32, // top
     };
 
-    let y1 = base_y - 4;
-    let y2 = base_y + opts.size as i32 - 2;
+    let y1 = base_y - 6;
+    let y2 = base_y + opts.size as i32 - 8;
 
     let line1_el = format!(
         r#"<text x="72" y="{y}" text-anchor="middle" font-family="sans-serif" font-size="{sz}" font-weight="bold" fill="{c}">{t}</text>"#,
